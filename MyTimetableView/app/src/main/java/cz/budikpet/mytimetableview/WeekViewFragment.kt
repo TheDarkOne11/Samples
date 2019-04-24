eb11008dbf39b2d01edaa7e6fd478239f04ebba4
PetrB <petr@Petr-MacBook-Pro.local>
2019-04-24 21:33:43 +0200

    Adding lone unoverlapping event somewhat works.

--- MyTimetableView/app/src/main/java/cz/budikpet/mytimetableview/WeekViewFragment.kt
+++ MyTimetableView/app/src/main/java/cz/budikpet/mytimetableview/WeekViewFragment.kt
@@ -19,6 +19,9 @@ import cz.budikpet.mytimetableview.data.TimetableEvent
 import kotlinx.android.synthetic.main.fragment_weekview_list.view.*
 import kotlinx.android.synthetic.main.week_row.view.*
 import org.joda.time.DateTime
+import org.joda.time.Days
+import org.joda.time.LocalTime
+import org.joda.time.Minutes
 
 /**
  * A fragment representing a list of Items.
@@ -31,13 +34,17 @@ class WeekViewFragment : Fragment() {
     private val events = mutableListOf<TimetableEvent>()
 
     private var eventsColumnsCount = MAX_COLUMN
+    private val eventPadding by lazy { 2f.toDp(context!!) }
     private var firstDate = DateTime()
     private val dpPerMinRatio = 1
     private val numOfLessons by lazy { sharedPreferences.getInt(SharedPreferencesKeys.NUM_OF_LESSONS.toString(), 0) }
-    private val lessonsStartTime by lazy { sharedPreferences.getInt(SharedPreferencesKeys.LESSONS_START_TIME.toString(), 0) }
     private val breakLength by lazy { sharedPreferences.getInt(SharedPreferencesKeys.LENGTH_OF_BREAK.toString(), 0) }
     private val lessonLength by lazy { sharedPreferences.getInt(SharedPreferencesKeys.LENGTH_OF_LESSON.toString(), 0) }
-    private val eventPadding by lazy { 2f.toDp(context!!) }
+    private val lessonsStartTime by lazy {
+        LocalTime().withMillisOfDay(
+            sharedPreferences.getInt(SharedPreferencesKeys.LESSONS_START_TIME.toString(), 0)
+        )
+    }
 
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var eventsColumns: List<ConstraintLayout>
@@ -65,14 +72,16 @@ class WeekViewFragment : Fragment() {
         val listLayout = layout.rowsList
 
         // Store references to dynamic event columns
-        eventsColumns = listOf(layout.eventColumn1, layout.eventColumn2, layout.eventColumn3, layout.eventColumn4,
-            layout.eventColumn5, layout.eventColumn6, layout.eventColumn7)
+        eventsColumns = listOf(
+            layout.eventColumn1, layout.eventColumn2, layout.eventColumn3, layout.eventColumn4,
+            layout.eventColumn5, layout.eventColumn6, layout.eventColumn7
+        )
 
         // Create rows
-        val currRowTime = DateTime().withMillisOfDay(lessonsStartTime)
-        for(i in 0 until numOfLessons) {
+        val currRowTime = DateTime().withTime(lessonsStartTime)
+        for (i in 0 until numOfLessons) {
             val rowView = getTimeRow(inflater)  // TODO: Create copies of this view?
-            rowView.timeTextView.text = currRowTime.plusMinutes(i*(lessonLength + breakLength)).toString("HH:mm")
+            rowView.timeTextView.text = currRowTime.plusMinutes(i * (lessonLength + breakLength)).toString("HH:mm")
 
             listLayout.addView(rowView)
         }
@@ -86,9 +95,10 @@ class WeekViewFragment : Fragment() {
     private fun getTimeRow(inflater: LayoutInflater): View {
         val rowView = inflater.inflate(R.layout.week_row, null, false)
 
-        val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        layoutParams.bottomMargin = (breakLength*dpPerMinRatio).toFloat().toDp(context!!)
-        layoutParams.height = (lessonLength*dpPerMinRatio).toFloat().toDp(context!!)
+        val layoutParams =
+            LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams.bottomMargin = (breakLength * dpPerMinRatio).toFloat().toDp(context!!)
+        layoutParams.height = (lessonLength * dpPerMinRatio).toFloat().toDp(context!!)
         rowView.layoutParams = layoutParams
 
         for (i in 1..MAX_COLUMN) {
@@ -151,44 +161,92 @@ class WeekViewFragment : Fragment() {
         val event1Start = firstDate.withTime(11, 0, 0, 0)
         val event1End = event1Start.plusMinutes(90)
 
-        val event1 = TimetableEvent(room = "T9:151", fullName = "BI-BAP", acronym = "BI-BAP_1", capacity = 90,
-            starts_at = event1Start, ends_at = event1End, event_type = EventType.COURSE_EVENT, teachers = arrayListOf("balikm"),
-            occupied = 49)
+        val event1 = TimetableEvent(
+            room = "T9:151",
+            fullName = "BI-BAP",
+            acronym = "BI-BAP_1",
+            capacity = 90,
+            starts_at = event1Start,
+            ends_at = event1End,
+            event_type = EventType.COURSE_EVENT,
+            teachers = arrayListOf("balikm"),
+            occupied = 49
+        )
 
         val event2Start = event1Start.withTime(9, 15, 0, 0)
         val event2End = event2Start.plusMinutes(195)
 
-        val event2 = TimetableEvent(room = "T9:153", fullName = "BI-END", acronym = "BI-END_2", capacity = 90,
-            starts_at = event2Start, ends_at = event2End, event_type = EventType.COURSE_EVENT, teachers = arrayListOf("bulim"),
-            occupied = 49)
+        val event2 = TimetableEvent(
+            room = "T9:153",
+            fullName = "BI-END",
+            acronym = "BI-END_2",
+            capacity = 90,
+            starts_at = event2Start,
+            ends_at = event2End,
+            event_type = EventType.COURSE_EVENT,
+            teachers = arrayListOf("bulim"),
+            occupied = 49
+        )
 
         val event3Start = firstDate.withTime(9, 15, 0, 0).plusDays(3)
         val event3End = event3Start.plusMinutes(195)
 
-        val event3 = TimetableEvent(room = "T9:153", fullName = "BI-LONE", acronym = "BI-LONE_3", capacity = 90,
-            starts_at = event3Start, ends_at = event3End, event_type = EventType.COURSE_EVENT, teachers = arrayListOf("bulim"),
-            occupied = 49)
+        val event3 = TimetableEvent(
+            room = "T9:153",
+            fullName = "BI-LONE",
+            acronym = "BI-LONE_3",
+            capacity = 90,
+            starts_at = event3Start,
+            ends_at = event3End,
+            event_type = EventType.COURSE_EVENT,
+            teachers = arrayListOf("bulim"),
+            occupied = 49
+        )
 
         val event4Start = firstDate.withTime(11, 0, 0, 0).plusDays(5)
         val event4End = event4Start.plusMinutes(90)
 
-        val event4 = TimetableEvent(room = "T9:151", fullName = "BI-BAP", acronym = "BI-BAP_4", capacity = 90,
-            starts_at = event4Start, ends_at = event4End, event_type = EventType.COURSE_EVENT, teachers = arrayListOf("balikm"),
-            occupied = 49)
+        val event4 = TimetableEvent(
+            room = "T9:151",
+            fullName = "BI-BAP",
+            acronym = "BI-BAP_4",
+            capacity = 90,
+            starts_at = event4Start,
+            ends_at = event4End,
+            event_type = EventType.COURSE_EVENT,
+            teachers = arrayListOf("balikm"),
+            occupied = 49
+        )
 
         val event5Start = event4Start.withTime(9, 15, 0, 0)
         val event5End = event5Start.plusMinutes(195)
 
-        val event5 = TimetableEvent(room = "T9:153", fullName = "BI-END", acronym = "BI-END_5", capacity = 90,
-            starts_at = event5Start, ends_at = event5End, event_type = EventType.COURSE_EVENT, teachers = arrayListOf("bulim"),
-            occupied = 49)
+        val event5 = TimetableEvent(
+            room = "T9:153",
+            fullName = "BI-END",
+            acronym = "BI-END_5",
+            capacity = 90,
+            starts_at = event5Start,
+            ends_at = event5End,
+            event_type = EventType.COURSE_EVENT,
+            teachers = arrayListOf("bulim"),
+            occupied = 49
+        )
 
         val event6Start = event4Start.withTime(14, 15, 0, 0)
         val event6End = event6Start.plusMinutes(90)
 
-        val event6 = TimetableEvent(room = "T9:153", fullName = "BI-LONE", acronym = "BI-LONE_6", capacity = 90,
-            starts_at = event6Start, ends_at = event6End, event_type = EventType.COURSE_EVENT, teachers = arrayListOf("bulim"),
-            occupied = 49)
+        val event6 = TimetableEvent(
+            room = "T9:153",
+            fullName = "BI-LONE",
+            acronym = "BI-LONE_6",
+            capacity = 90,
+            starts_at = event6Start,
+            ends_at = event6End,
+            event_type = EventType.COURSE_EVENT,
+            teachers = arrayListOf("bulim"),
+            occupied = 49
+        )
 
 
         events.add(event1)
@@ -209,12 +267,12 @@ class WeekViewFragment : Fragment() {
         val preparedCollection = events.map { return@map IndexedTimetableEvent(-1, it) }
 
         preparedCollection.forEach { indexedTimetableEvent1: IndexedTimetableEvent ->
-            if(indexedTimetableEvent1.index == -1) {
+            if (indexedTimetableEvent1.index == -1) {
                 indexedTimetableEvent1.index = currIndex
 
                 preparedCollection.forEach { indexedTimetableEvent2: IndexedTimetableEvent ->
-                    if(indexedTimetableEvent2.index == -1) {
-                        if(indexedTimetableEvent1.timetableEvent.overlapsWith(indexedTimetableEvent2.timetableEvent)) {
+                    if (indexedTimetableEvent2.index == -1) {
+                        if (indexedTimetableEvent1.timetableEvent.overlapsWith(indexedTimetableEvent2.timetableEvent)) {
                             indexedTimetableEvent2.index = currIndex
                         }
                     }
@@ -229,9 +287,9 @@ class WeekViewFragment : Fragment() {
                 val currEvents = mapEntry.value.map { it.timetableEvent }
 
                 // TODO: Call methods
-                if(currEvents.size == 1) {
+                if (currEvents.size == 1) {
                     // Add lone event
-
+                    addEvent(currEvents.first())
                 } else {
                     // Add overlapping event
                 }
@@ -239,30 +297,30 @@ class WeekViewFragment : Fragment() {
     }
 
     private fun addEvent(event: TimetableEvent) {
-        val startsDp = 100f.toDp(context!!)
-        val constLayout = eventsColumns.elementAt(1)
-
+        // Create event view
+        val startsDp = getEventViewStart(event)
         val eventView = TextView(context!!)
         eventView.text = event.acronym
         eventView.setBackgroundColor(Color.RED)
-        eventView.height = 100f.toDp(context!!)
-
-        val set = ConstraintSet()
+        eventView.height = getEventViewHeight(event)
 
         if (eventView.id == -1) {
             eventView.id = View.generateViewId()
         }
 
-        constLayout.addView(eventView)
+        // Add event view into the correct column
+        val column = getEventsColumn(event)
+        column.addView(eventView)
 
-        set.clone(constLayout)
+        val set = ConstraintSet()
+        set.clone(column)
 
-        set.connect(eventView.id, ConstraintSet.TOP, constLayout.id, ConstraintSet.TOP, startsDp)
+        set.connect(eventView.id, ConstraintSet.TOP, column.id, ConstraintSet.TOP, startsDp)
         set.constrainWidth(eventView.id, 0f.toDp(context!!))
-        set.connect(eventView.id, ConstraintSet.START, constLayout.id, ConstraintSet.START, eventPadding)
-        set.connect(eventView.id, ConstraintSet.END, constLayout.id, ConstraintSet.END, eventPadding)
+        set.connect(eventView.id, ConstraintSet.START, column.id, ConstraintSet.START, eventPadding)
+        set.connect(eventView.id, ConstraintSet.END, column.id, ConstraintSet.END, eventPadding)
 
-        set.applyTo(constLayout)
+        set.applyTo(column)
     }
 
     private fun addOverlappingEvents(constLayout: ConstraintLayout, events: Array<View>, startsDp: Array<Int>) {
@@ -300,12 +358,23 @@ class WeekViewFragment : Fragment() {
 
     }
 
-    private fun getEventViewHeight() {
-
+    private fun getEventsColumn(event: TimetableEvent): ConstraintLayout {
+        val index = Days.daysBetween(firstDate, event.starts_at).days
+        return eventsColumns.elementAt(index + 1)
     }
 
-    private fun getEventViewStart() {
+    private fun getEventViewHeight(event: TimetableEvent): Int {
+        return Minutes.minutesBetween(event.starts_at, event.ends_at)
+            .minutes
+            .toFloat()
+            .toDp(context!!)
+    }
 
+    private fun getEventViewStart(event: TimetableEvent): Int {
+        return Minutes.minutesBetween(event.starts_at.withTime(lessonsStartTime), event.starts_at)
+            .minutes
+            .toFloat()
+            .toDp(context!!)
     }
 
     // MARK: Initializers
